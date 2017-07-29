c1793144ad42ec82acaadfe1a4d0b1bef58410da
vic <vic@users.noreply.github.com>
2017-07-29 06:28:35 -0500

    Add license
    Closing #3

--- ido-better-flex.el
+++ ido-better-flex.el
@@ -1,5 +1,18 @@
 ;;; ido-better-flex.el --- A better flex (fuzzy) algorithm for Ido.
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Copyright 2012 Victor Hugo Borja.
 ;; Author: Victor Hugo Borja <vic.borja@gmail.com>
 ;; URL: http://github.com/vic/ido-better-flex
